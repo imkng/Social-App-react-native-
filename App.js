f7c6aaad59884169dd17c2043654002151c62890
Krishna <1india.kng@gamil.com>
2021-07-31 17:41:06 +0530

    Google and Facebook Authentication completed

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import Providers from './navigation';
+
+const App = () => {
+  console.log("App Started!!!!")
+  return <Providers />;
+  // return (
+  //   <View>
+  //     <Text>Hello World</Text>
+  //   </View>
+  // )
+  // return (
+  //   <Text>Helloo</Text>
+  // )
+}
+
+export default App;
