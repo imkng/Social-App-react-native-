2502cecbf911ca12fb7b000d9c870533c6a7fb39
Krishna <1india.kng@gamil.com>
2021-08-01 20:14:22 +0530

    User Profile UI Completed

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,14 +1,115 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, FlatList } from 'react-native'
 
+import PostCard from '../components/PostCard';
+import { Container } from '../styles/FeedStyles'
+import Icon from 'react-native-vector-icons/Ionicons';
+
+const Posts = [
+    {
+        id: 1,
+        userName: "Krishna",
+        userImg: require("../assets/users/user-3.jpg"),
+        postTime: "4 min ago",
+        post: "Hey I am using React Native for development",
+        postImg: require('../assets/posts/post-1.jpg'),
+        liked: true,
+        likes: '14',
+        comments: '5'
+    },
+    {
+        id: 2,
+        userName: "Krishna",
+        userImg: require("../assets/users/user-1.jpg"),
+        postTime: "4 min ago",
+        post: "Hey I am using React Native for development",
+        postImg: require('../assets/posts/post-1.jpg'),
+        liked: false,
+        likes: '0',
+        comments: '0'
+    },
+    {
+        id: 3,
+        userName: "Krishna",
+        userImg: require("../assets/users/user-3.jpg"),
+        postTime: "4 min ago",
+        post: "Hey I am using React Native for development",
+        postImg: 'none',
+        liked: false,
+        likes: '14',
+        comments: '5'
+    },
+    {
+        id: 4,
+        userName: "Krishna",
+        userImg: require("../assets/users/user-2.jpg"),
+        postTime: "4 min ago",
+        post: "Hey I am using React Native for development",
+        postImg: require('../assets/posts/post-1.jpg'),
+        liked: false,
+        likes: '14',
+        comments: '5'
+    },
+    {
+        id: 5,
+        userName: "Nill",
+        userImg: require("../assets/users/user-4.jpg"),
+        postTime: "4 min ago",
+        post: "Hey I am using React Native for development",
+        postImg: require('../assets/posts/post-1.jpg'),
+        liked: true,
+        likes: '14',
+        comments: '5'
+    },
+    {
+        id: 6,
+        userName: "Krishna",
+        userImg: require("../assets/users/user-3.jpg"),
+        postTime: "4 min ago",
+        post: "Hey I am using React Native for development",
+        postImg: require('../assets/posts/post-1.jpg'),
+        liked: false,
+        likes: '14',
+        comments: '5'
+    },
+    {
+        id: 7,
+        userName: "Raghav",
+        userImg: require("../assets/users/user-4.jpg"),
+        postTime: "4 min ago",
+        post: "Hey I am using React Native for development",
+        postImg: require('../assets/posts/post-1.jpg'),
+        liked: true,
+        likes: '14',
+        comments: '5'
+    },
+    {
+        id: 8,
+        userName: "Krishna",
+        userImg: require("../assets/users/user-3.jpg"),
+        postTime: "4 min ago",
+        post: "Hey I am using React Native for development",
+        postImg: require('../assets/posts/post-1.jpg'),
+        liked: true,
+        likes: '14',
+        comments: '5'
+    },
+]
 const HomeScreen = () => {
     return (
-        <View>
-            <Text>Home Screen</Text>
-        </View>
+        <Container>
+            <FlatList
+                data={Posts}
+                renderItem={({ item }) => <PostCard item={item} />}
+                keyExtractor={item => item.id}
+                showsVerticalScrollIndicator={false}
+            />
+        </Container>
+
+
     )
 }
 
 export default HomeScreen
 
-const styles = StyleSheet.create({})
+
---
+++ styles/AddPost.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const InputWrapper = styled.View`
+    flex : 1;
+    justify-content : center;
+    align-items : center;
+    width : 100%;
+    backgroundColor : #2e64e515;
+
+`;
+
+export const InputField = styled.TextInput`
+    justify-content : center;
+    align-items : center;
+    font-size : 24px;
+    text-align : center;
+    width : 90%;
+
+`;
